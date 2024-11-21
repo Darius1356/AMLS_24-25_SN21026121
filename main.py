487cacf96a1c3281803102faf13d45de886eb39c
Darius <dariusjoshi@hotmail.co.uk>
2024-11-21 13:09:23 +0000

    Added imports

---
+++ main.py
@@ -0,0 +1,10 @@
+from tqdm import tqdm
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import torchvision.transforms as transforms
+
+import medmnist
+from medmnist import INFO, Evaluator
